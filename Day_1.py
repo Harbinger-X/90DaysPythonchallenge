af255f5add8294d170cf37d467bcde520119adc4
Hrbinger-X <skyphaceyaw@gmail.com>
2025-01-06 22:21:45 +0000

    Added Day 1 Python script

---
+++ Day_1.py
@@ -0,0 +1,13 @@
+# A simple hello world  script 
+print("Hello world's Day 1")
+#Basic Arithmetic
+result1 = 2 + 2  # Addition
+result2 = 5 * 3  # Multiplication
+result3 = 10 - 4 # Subtraction
+result4 = 15 / 3 # Division
+
+# Print the results
+print("2 + 2 =", result1)
+print("5 * 3 =", result2)
+print("10 - 4 =", result3)
+print("15 / 3 =", result4)
