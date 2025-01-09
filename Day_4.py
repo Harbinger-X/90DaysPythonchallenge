c9c9d356faeeae8e4adad502004b2da2996b8082
Hrbinger-X <skyphaceyaw@gmail.com>
2025-01-09 17:36:02 +0000

    Added Day 4 Python script

---
+++ Day_4.py
@@ -0,0 +1,8 @@
+# A simple countdown using while loops
+count = 10
+
+# Countdown loop
+while count >= 1:  # Continue as long as count is greater than or equal to 1
+    print(count)   # Print the current number
+    count -= 1     # Decrease the number by 1
+
